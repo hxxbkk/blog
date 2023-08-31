30b7eba7c6b81461019e5721b5cdb0d58d7f39d1
hyobin <hbaloha@naver.com>
2023-08-31 17:13:23 +0900

    setting

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,25 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
+
+const path = require("path")
+
+//Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
