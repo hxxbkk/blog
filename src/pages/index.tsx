81de863692c233d47aba8317ba26fe1e8ba1c90b
hyobin <hbaloha@naver.com>
2023-09-13 15:29:12 +0900

    Separating overlapping parts from the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useMemo } from 'react'
 import styled from '@emotion/styled'
-import GlobalStyle from 'components/Common/Globalstyle'
-import Footer from 'components/Common/Footer'
+// import GlobalStyle from 'components/Common/Globalstyle'
+// import Footer from 'components/Common/Footer'
 import CategoryList from 'components/Main/CategoryList'
 import Introduction from 'components/Main/Introduction'
 import PostList, { PostType } from 'components/Main/PostList'
@@ -9,6 +9,7 @@ import { graphql } from 'gatsby'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
 import { PostListItemType } from 'types/PostItem.types'
 import queryString, { ParsedQuery } from 'query-string'
+import Template from 'components/Common/Template'
 
 // const CATEGORY_LIST = {
 //   All: 5,
@@ -78,16 +79,14 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
     [],
   )
   return (
-    <Container>
-      <GlobalStyle />
+    <Template>
       <Introduction profileImage={gatsbyImageData} />
       <CategoryList
         selectedCategory={selectedCategory}
         categoryList={categoryList}
       />
       <PostList selectedCategory={selectedCategory} posts={edges} />
-      <Footer />
-    </Container>
+    </Template>
   )
 }
 
